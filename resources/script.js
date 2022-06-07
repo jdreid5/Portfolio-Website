39717a2b3f0fb4307196caa5ef64c1c5c30a66da
jdreid5 <jdreid5@aol.com>
2022-06-07 12:32:22 +0100

    major updates, add image, add css animation, add js scripts

---
+++ resources/script.js
@@ -0,0 +1,18 @@
+const arrow = document.querySelector('#arrow');
+
+function moveDown() {
+    arrow.style.animationPlayState = 'paused';
+    window.scrollTo({
+        top: 650,
+        left: 0,
+        behavior: 'smooth'
+      });
+}
+
+function cursorChange() {
+    this.style.cursor = 'pointer';
+}
+
+document.querySelector('#onclick').onclick = moveDown;
+document.querySelector('#onclick').onmouseover = cursorChange;
+
